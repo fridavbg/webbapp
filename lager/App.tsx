e99788df22f9f7a7472e43e7b412a499e093fd3b
fridavbg <fperssontech@gmail.com>
2022-04-02 16:59:01 +0200

    base styling

--- lager/App.tsx
+++ lager/App.tsx
@@ -1,33 +1,69 @@
+import { StatusBar } from "expo-status-bar";
+import {
+    Image,
+    StyleSheet,
+    Text,
+    TouchableOpacity,
+    Alert,
+    View,
+    ScrollView,
+} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useDeviceOrientation } from "@react-native-community/hooks";
 
-import { StatusBar } from 'expo-status-bar';
-import { Image, StyleSheet, Text, View } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import Stock from "./components/Stock.tsx";
 
-import Stock from './components/Stock.tsx';
-
-import warehouse from './assets/warehouse.jpeg';
+import warehouse from "./assets/warehouse.jpeg";
 
 export default function App() {
-  return (
-    <SafeAreaView style={styles.container}>
-    <View style={styles.container}>
-      <Text style={{ color: '#33c', fontSize: 42 }}>Lager-Appen</Text>
-        <Image source={warehouse} style={{ width: 320, height: 240 }} />
-        <Stock />
-      <StatusBar style="auto" />
-      </View>
-      </SafeAreaView>
-  );
+    const { landscape } = useDeviceOrientation();
+    return (
+        <SafeAreaView style={styles.container}>
+            <ScrollView style={styles.base}>
+                <Text
+                    style={{
+                        color: "#A5C882",
+                        fontSize: 42,
+                        textAlign: "center",
+                        paddingTop: 15,
+                    }}
+                    numberOfLines={1}
+                >
+                    Lager-Appen
+                </Text>
+                <TouchableOpacity
+                    onPress={() => {
+                        Alert.alert("Image of a", "Warehouse");
+                    }}
+                >
+                    <Image
+                        source={warehouse}
+                        style={{
+                            width: landscape ? 350 : 280,
+                            height: landscape ? 150 : 140,
+                            margin: 15,
+                            borderRadius: 10,
+                        }}
+                    />
+                </TouchableOpacity>
+                <Stock />
+                <StatusBar style="auto" />
+            </ScrollView>
+        </SafeAreaView>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  base: {
-    flex: 1,
-    backgroundColor: '#fff',
-    paddingLeft: 12,
-    paddingRight: 12,
-  }
+    container: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    base: {
+        flex: 1,
+        backgroundColor: "#4E6766",
+        paddingLeft: 10,
+        paddingRight: 10,
+        borderRadius: 10,
+    },
 });
--- lager/components/Stock.tsx
+++ lager/components/Stock.tsx
@@ -1,30 +1,49 @@
-import { useState, useEffect } from 'react';
-import { Text, View } from 'react-native';
+import { useState, useEffect, Component } from "react";
+import { StyleSheet, Text, View } from "react-native";
+
 import config from "../config/config.json";
 
 function StockList() {
-  const [products, setProducts] = useState([]);
+    const [products, setProducts] = useState([]);
 
-  useEffect(() => {
-    fetch(`${config.base_url}/products?api_key=${config.api_key}`)
-      .then(response => response.json())
-      .then(result => setProducts(result.data));
-  }, []);
-    
-  const list = products.map((product, index) => <Text key={index}>{ product.name } - { product.stock }</Text>);
+    useEffect(() => {
+        fetch(`${config.base_url}/products?api_key=${config.api_key}`)
+            .then((response) => response.json())
+            .then((result) => setProducts(result.data));
+    }, []);
 
-  return (
-    <View>
-      {list}
-    </View>
-  );
+    const list = products.map((product, index) => (
+        <Text key={index} style={styles.stock}>
+            {product.name} - {product.stock}
+        </Text>
+    ));
+
+    return <View>{list}</View>;
 }
 
 export default function Stock() {
     return (
-      <View>
-        <Text style={{color: '#333', fontSize: 24}}>Lagerförteckning</Text>
-        <StockList/>
-      </View>
+        <View>
+            <Text style={styles.title}>Lagerförteckning</Text>
+            <StockList />
+        </View>
     );
-  }
+}
+
+const styles = StyleSheet.create({
+    title: {
+        color: "#ACD7EC",
+        fontSize: 34,
+        textAlign: "center",
+    },
+    stock: {
+        borderWidth: 3,
+        borderStyle: "solid",
+        borderRadius: 10,
+        fontSize: 20,
+        color: "#D6EDFF",
+        marginBottom: 5,
+        padding: 5,
+        textAlign: "center",
+    },
+});
