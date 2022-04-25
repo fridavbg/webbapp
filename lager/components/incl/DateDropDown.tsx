76a40d28a80c5fe2ee96170cfea9b4e58b4a9464
fridavbg <fperssontech@gmail.com>
2022-04-25 15:26:07 +0200

    DateDropDown komponent

---
+++ lager/components/incl/DateDropDown.tsx
@@ -0,0 +1,44 @@
+import DateTimePicker from "@react-native-community/datetimepicker";
+import { useState } from "react";
+import {
+    ScrollView,
+    Text,
+    TouchableOpacity,
+    View,
+    Platform,
+} from "react-native";
+import { Base, Typography, Form } from "../../styles";
+import Moment from "moment";
+
+export default function DateDropDown(props) {
+    const [dropDownDate, setDropDownDate] = useState<Date>(new Date());
+    const [show, setShow] = useState<Boolean>(false);
+
+    const showDatePicker = () => {
+        setShow(true);
+    };
+    return (
+        <View>
+            {Platform.OS === "android" && (
+                <TouchableOpacity
+                    style={Base.button}
+                    onPress={showDatePicker}
+                    onChange={(_event: any, date: any) => {
+                        setDropDownDate(date || new Date());
+                    }}
+                >
+                    <Text style={Typography.btnText}>Visa datumväljare</Text>
+                </TouchableOpacity>
+            )}
+            {(show || Platform.OS === "ios") && (
+                <DateTimePicker
+                    onChange={(_event: any, date: any) => {
+                        setDropDownDate(date || new Date());
+                        setShow(false);
+                    }}
+                    value={dropDownDate}
+                />
+            )}
+        </View>
+    );
+}
--- lager/components/products/ProductsDropDown.tsx
+++ lager/components/products/ProductsDropDown.tsx
@@ -1,31 +1,30 @@
 import { Picker } from "@react-native-picker/picker";
 import { useState, useEffect } from "react";
 
-import orderModel from "../../models/orders";
+import productModel from "../../models/products";
 
 export default function ProductDropDown(props) {
-    const [order, setOrder] = useState<Order[]>([]);
+    const [products, setProducts] = useState<Products[]>([]);
     let productsHash: any = {};
 
     useEffect(async () => {
-      //  setProducts(await productModel.getProducts());
+        setProducts(await productModel.getProducts());
     }, []);
 
     const itemsList = products.map((prod, index) => {
         productsHash[prod.id] = prod;
-        // return <Picker.Item key={index} label={prod.name} value={prod.id} />;
-        return null;
+        return <Picker.Item key={index} label={prod.name} value={prod.id} />;
     });
 
     return (
         <Picker
             selectedValue={props.delivery?.product_id}
             onValueChange={(itemValue) => {
-                // props.setDelivery({ ...props.delivery, product_id: itemValue });
-                // props.setCurrentProduct(productsHash[itemValue]);
+                props.setDelivery({ ...props.delivery, product_id: itemValue });
+                props.setCurrentProduct(productsHash[itemValue]);
             }}
         >
-            { itemsList}
+            {itemsList}
         </Picker>
     );
 }
