cace4b5f0bdf171a6f2fe4d6b6af721803bfa2f2
fridavbg <fperssontech@gmail.com>
2022-04-25 15:45:56 +0200

    OrderDropDown init

--- lager/components/invoices/InvoiceForm.tsx
+++ lager/components/invoices/InvoiceForm.tsx
@@ -1,11 +1,13 @@
 import { ScrollView, Text } from "react-native";
 import { Base, Typography, Form } from "../../styles";
 import DateDropDown from "../incl/DateDropDown";
+import OrderDropDown from "../order/OrderDropDowm";
 
 export default function InvoiceForm({ navigation }) {
     return (
         <ScrollView style={{ ...Base.container }}>
             <Text style={{ ...Typography.title }}>INVOICE FORM</Text>
+            <OrderDropDown />
             <DateDropDown />
         </ScrollView>
     );
--- lager/components/order/OrderDropDowm.tsx
+++ lager/components/order/OrderDropDowm.tsx
@@ -1,6 +1,33 @@
 import { Picker } from "@react-native-picker/picker";
 import { useState, useEffect } from "react";
+import { View, Text, TouchableOpacity, ScrollView } from "react-native";
+import orderModel from "../../models/orders";
+import { Base } from "../../styles";
 
-export default function OrderDropDown(props) { 
-    const 
+export default function OrderDropDown(props) {
+    const [orders, setOrders] = useState<Orders[]>([]);
+    let orderHash: any = {};
+
+    useEffect(async () => {
+        setOrders(await orderModel.getOrders());
+    });
+
+    const orderList = orders.map((order, index) => {
+        if (order.status !== "Fakturerad") {
+            return (
+                <Picker.Item key={index} label={order.name} value={order.id} />
+            );
+        }
+        return <Text>All orders have been invoiced</Text>;
+    });
+
+    return <Picker
+        selectedValue={props.order?.order_id}
+        onValueChange={(itemValue) => { 
+            props.setOrders({ ...props.order, order_id: itemValue });
+           // props.setCurrentOrder[orderHash[itemValue]];
+        }}
+    >
+        {orderList}
+    </Picker>;
 }
