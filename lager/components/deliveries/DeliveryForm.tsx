9831dcf6c8e982efb87687d66c2403ee5e33e5ee
fridavbg <fperssontech@gmail.com>
2022-10-03 10:12:03 +0200

    flashmessages DeliveryForm

--- lager/components/deliveries/DeliveryForm.tsx
+++ lager/components/deliveries/DeliveryForm.tsx
@@ -5,22 +5,37 @@ import Delivery from "../../interfaces/delivery";
 import deliveryModel from "../../models/deliveries";
 import productModel from "../../models/products";
 import ProductDropDown from "../products/ProductsDropDown";
-import DateDropDown from "../deliveries/DeliveryDateDropDown.tsx";
+import Product from "../products/";
+import DateDropDown from "../deliveries/DeliveryDateDropDown";
+import { showMessage } from "react-native-flash-message";
 
 export default function DeliveryForm({ navigation }) {
     const [delivery, setDelivery] = useState<Partial<Delivery>>({});
     const [currentProduct, setCurrentProduct] = useState<Partial<Product>>({});
 
     async function addDelivery(props) {
-        await deliveryModel.addDelivery(delivery);
-        const updatedProduct = {
-            ...currentProduct,
-            stock: (currentProduct.stock || 0) + (delivery.amount || 0),
-        };
+        if (
+            props.product_id &&
+            props.delivery_date &&
+            props.amount &&
+            props.comment
+        ) {
+            await deliveryModel.addDelivery(delivery);
+            const updatedProduct = {
+                ...currentProduct,
+                stock: (currentProduct.stock || 0) + (delivery.amount || 0),
+            };
 
-        await productModel.updateProduct(updatedProduct);
+            const result = await productModel.updateProduct(updatedProduct);
 
-        navigation.navigate("DeliveryList", { reload: true });
+            navigation.navigate("DeliveryList", { reload: true });
+        } else {
+            showMessage({
+                message: "Saknas",
+                description: "Är alla fält ifyllda?",
+                type: "warning",
+            });
+        }
     }
 
     return (
