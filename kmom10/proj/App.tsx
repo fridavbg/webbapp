8a2b98a1922b78ababc2a9b77bfaa4c477ff9b5c
fridavbg <fperssontech@gmail.com>
2023-06-20 22:20:03 +0200

    Init dog component

--- kmom10/proj/App.tsx
+++ kmom10/proj/App.tsx
@@ -1,20 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import Dogs from './src/components/Dogs';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.base}>
+        <Text style={{color: '#33c', fontSize: 24}}>Match and Adopt a Dog</Text>
+        <Dogs />
+        <StatusBar style="auto" />
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  base: {
+    flex: 1,
+    backgroundColor: '#fff',
+    padding: 12,
+    margin: 15,
+  }
 });
---
+++ kmom10/proj/src/components/Dogs.tsx
@@ -0,0 +1,7 @@
+import { Text } from "react-native";
+
+export default function Dog() { 
+    return (
+        <Text style={{color: '#333', fontSize: 24}}>Dogs</Text>
+    )
+};
